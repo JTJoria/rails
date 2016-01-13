b368bc6da4269144417bfa2c428bb33393d5a790
TIM JORIA <jtjoria@gmail.com>
2016-01-12 18:27:52 -0600

    review

--- lotto-app/app/controllers/numbers_controller.rb
+++ lotto-app/app/controllers/numbers_controller.rb
@@ -1,7 +1,11 @@
 class NumbersController < ApplicationController
 
   def lotto
-    win = []
-    
+    @win = []
+
+    6.times do
+      random_number = rand(1..60)
+      @win << random_number
+    end
   end
 end
